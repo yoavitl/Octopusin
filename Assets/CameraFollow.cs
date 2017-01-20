74c8bd4988140ef6a214d8b20300a3b527994e62
toviimaster <master.tavas@gmail.com>
2017-01-20 15:54:55 +0200

    add some elements to the project

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 public class CameraFollow : MonoBehaviour
-{
+{	
 	private bool shaking;
 	private Transform _transform;
 
---
+++ Assets/Scripts/movingObj.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movingObj : MonoBehaviour {
+	public Camera cam;
+	private Transform _transform;
+	public float growthRate;
+
+	void Start () {
+		_transform = this.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.anyKeyDown) { 
+			_transform.localPosition = new Vector3 (_transform.position.x, _transform.position.y, _transform.position.z + 1);
+			_transform.localScale += new Vector3 (1F * growthRate , 1F * growthRate, 0);
+		}
+
+		if (_transform.position.z > cam.transform.position.z) 
+			Destroy (gameObject);	
+	}
+}
