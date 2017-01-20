4436a0a557b87d53b5439be5a8539a5372171383
toviimaster <master.tavas@gmail.com>
2017-01-20 13:00:21 +0200

    changed scripts and added octupus arm script

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -1,18 +1,53 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class CameraFollow : MonoBehaviour {
-
-	public BoxCollider2D toFollow;
-
-	// Use this for initialization
-	void Start () {
-		
+﻿using UnityEngine;
+using System.Collections;
+public class CameraFollow : MonoBehaviour
+{
+	private Vector3 originPosition;
+	private Quaternion originRotation;
+	public float shake_decay;
+	public float shake_intensity;
+	private bool shaking;
+	private Transform _transform;
+	void Start (){
+			Shake ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void OnEnable() {
+		_transform = transform;
 	}
+
+	void Update (){
+		if(!shaking)
+			return;
+
+		if (shake_intensity > 0f){
+			_transform.localPosition = originPosition + Random.insideUnitSphere * shake_intensity;
+			_transform.localRotation = new Quaternion(
+				originRotation.x + Random.Range (-shake_intensity,shake_intensity) * .2f,
+				originRotation.y + Random.Range (-shake_intensity,shake_intensity) * .2f,
+				originRotation.z + Random.Range (-shake_intensity,shake_intensity) * .2f,
+				originRotation.w + Random.Range (-shake_intensity,shake_intensity) * .2f);
+			shake_intensity -= shake_decay;
+		} 
+		else {
+			Debug.Log("stopped shaking");
+			shaking = false;
+			_transform.localPosition = originPosition;
+			_transform.localRotation = originRotation;
+
+		}
+
+	}
+
+	void Shake(){
+		if(!shaking) {
+			originPosition = _transform.localPosition;
+			originRotation = _transform.localRotation;
+			}
+		shaking = true;
+		shake_intensity = .1f;
+		shake_decay = 0.002f;
+
+	}
+
 }
