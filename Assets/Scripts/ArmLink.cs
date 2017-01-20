578dff1e701d528f6850bcb5297720834e157ecb
toviimaster <master.tavas@gmail.com>
2017-01-20 13:19:24 +0200

    my changes olus armlink script

---
+++ Assets/Scripts/ArmLink.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmLink : MonoBehaviour {
+
+	public int armIndex;
+	public float slowFactor = 3;
+
+	// Use this for initialization
+	void Start () {
+		updateIndex ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float x = (Time.time / slowFactor) % (2f * Mathf.PI);
+		this.transform.position = new Vector2 (armIndex, Mathf.Sin (x + armIndex));
+	}
+		
+
+	public int updateIndex(){
+		armIndex = transform.GetSiblingIndex ();
+		return armIndex;
+	}
+}
